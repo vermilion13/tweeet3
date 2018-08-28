31ad2ca2b28d85e9d24a75975bffec152fe75326
Gen Chida <varm666@mist.dti.ne.jp>
2018-08-28 15:08:45 +0000

    今度こそ

--- db/migrate/20180828112644_add_column_tweeets.rb
+++ db/migrate/20180828112644_add_column_tweeets.rb
@@ -1,5 +1,4 @@
 class AddColumnTweeets < ActiveRecord::Migration[5.1]
   def change
-    add_column :tweeets, :user, :string
   end
 end
--- db/migrate/20180828112916_remove_author_from_tweeets.rb
+++ db/migrate/20180828112916_remove_author_from_tweeets.rb
@@ -1,5 +1,4 @@
 class RemoveAuthorFromTweeets < ActiveRecord::Migration[5.1]
   def change
-    remove_column :tweeets, :user, :string
   end
 end
